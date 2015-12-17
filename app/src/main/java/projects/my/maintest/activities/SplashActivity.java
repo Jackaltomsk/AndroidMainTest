1ef8510e9de577063755864401015f13c0b44075
Jackaltomsk <jackal@sibmail.com>
2015-12-17 20:33:34 +0600

    + сплэш-скрин;

--- app/src/main/java/projects/my/maintest/activities/SplashActivity.java
+++ app/src/main/java/projects/my/maintest/activities/SplashActivity.java
@@ -1,15 +1,46 @@
 package projects.my.maintest.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 import projects.my.maintest.R;
 
+@EActivity(R.layout.activity_splash)
 public class SplashActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+    @ViewById(R.id.splash_id)
+    View layout;
+
+    @AfterViews
+    void init() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.splah_fade_in);
+        final Context context = this;
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(context, MainActivity_.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        layout.startAnimation(anim);
     }
 }
